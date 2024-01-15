56b1ea0f90630abb5668b4043ba6d7a2ec540b2a
Hariprasad <36653879+hariprasad-thma@users.noreply.github.com>
2024-01-15 11:54:21 +0530

    basic gradle infrastructure

---
+++ src/main/java/com/springcloudtemplate/springcloudtemplate/SpringcloudtemplateApplication.java
@@ -0,0 +1,13 @@
+package com.springcloudtemplate.springcloudtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudtemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudtemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcloudtemplate/springcloudtemplate/SpringcloudtemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudtemplate.springcloudtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudtemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
